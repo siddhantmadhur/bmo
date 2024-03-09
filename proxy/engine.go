baa94ae85259e340a906e7775b0ca50ee4d1eab6
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-03-08 16:42:22 -0800

    Created basic websocket for proxy server

--- proxy/engine.go
+++ proxy/engine.go
@@ -3,9 +3,12 @@ package proxy
 import (
 	"embed"
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	"bmo.siddhantsoftware.com/config"
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
 )
@@ -24,7 +27,37 @@ func (p *Proxy) Start(c *config.Config) {
     fmt.Println("Proxy starting...") 
 
     app := fiber.New()
-    
+   
+    app.Use("/ws", func(c *fiber.Ctx) error {
+        // IsWebSocketUpgrade returns true if the client
+        // requested upgrade to the WebSocket protocol.
+        if websocket.IsWebSocketUpgrade(c) {
+            c.Locals("allowed", true)
+            return c.Next()
+        }
+        return fiber.ErrUpgradeRequired
+    })
+
+    app.Get("/ws/proxy", websocket.New(func(c *websocket.Conn) {
+        log.Println(c.Locals("allowed"))  // true
+        log.Println(c.Params("id"))       // 123
+        log.Println(c.Query("v"))         // 1.0
+        log.Println(c.Cookies("session")) // ""
+
+        // websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+        var (
+            err error
+        )
+        
+        for {
+            time.Sleep(time.Second * 2)
+            if err = c.WriteMessage(websocket.TextMessage, []byte("This is a test")); err != nil {
+                log.Println("write:", err)
+                break
+            }
+        }
+    }))
+
     app.All("/*", func(c *fiber.Ctx) error {
         if err := proxy.Do(c, p.config.WebServerUrl + c.Path()); err != nil {
             return err
--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,1 +1,7 @@
 console.log("BMO: Proxy loaded :D")
+
+const websocket = new WebSocket("ws://127.0.0.1:9090/ws/proxy")
+
+websocket.addEventListener("message", (event) => {
+    console.log("BMO: " + event)
+})
