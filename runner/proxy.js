f797d859443e287071f05baeb4defc2cab58331f
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-03-08 17:17:18 -0800

    Refreshes page but with slight delay

--- runner/proxy.js
+++ runner/proxy.js
@@ -2,6 +2,8 @@ console.log("BMO: Proxy loaded :D")
 
 const websocket = new WebSocket("ws://127.0.0.1:9090/ws/proxy")
 
-websocket.addEventListener("message", (event) => {
-    console.log("BMO: " + event)
+websocket.addEventListener("message", async () => {
+    setTimeout(function() {
+        location.replace(location.href)
+    }, 1500)
 })
