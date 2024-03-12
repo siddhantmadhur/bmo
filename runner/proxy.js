962451567626c93a4a23ba6f8e2df0d8e36676a2
SiddhantMadhur <siddhant.madhur@gmail.com>
2024-03-12 13:15:34 -0700

    Removed unnecessary check on proxy.js

--- runner/proxy.js
+++ runner/proxy.js
@@ -4,13 +4,8 @@ console.log("Hello world");
 const ws = new WebSocket("ws://localhost:9090/ws/proxy")
 
 ws.addEventListener("message", async () => {
-    console.log("Hello")
-    var finish = false
-    while (!finish) {
-        const res = await fetch("http://localhost:9090");
-        if (res.ok) {
-            document.body.innerHTML = await res.text()
-            finish = true
-        }
-    }
+
+    const res = await fetch("http://localhost:9090");
+    document.body.innerHTML = await res.text()
+    finish = true
 })
